5cf0129a4cd76233529fb3893f55417baed2bc70
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-11-03 21:58:19 -0500

    User class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-// Alguien revise mi repositorio, asi sea para insultarme.
+// Please begin
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome! We're working hard to make you use wisely your bucks");
-            System.out.println("i = " + i);
-        }
     }
 }
