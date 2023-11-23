c75bb12c702ecaf98d20fc4b6de7d342e4d6dec9
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-11-23 11:04:53 -0500

    Relocated Main, new MenuHandler

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,7 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+      /*  // Inicializar instancias de las clases de la capa de datos
+        data.UserData userData = new data.UserData();
+        data.EntryData entryData = new data.EntryData();
 
+        // Inicializar instancias de las clases de la capa de negocio
+        business.UserManager userManager = new business.UserManager(userData);
+        business.EntryManager entryManager = new business.EntryManager(entryData);
+*/
         // Inicializar el manejador de menú
         presentation.MenuHandler menuHandler = new presentation.MenuHandler(scanner);
 
