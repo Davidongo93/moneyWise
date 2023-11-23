66a4cc9cfffdaf7b468d4cd9823aa26c39b8b38e
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-11-23 13:07:32 -0500

    enum

--- src/Model/Entry.java
+++ src/Model/Entry.java
@@ -3,15 +3,75 @@ package Model;
 import java.util.Date;
 
 public class Entry {
+    private String id;
     private Date date;
     private String description;
     private double amount;
+    private EntryType type;  // Enum para representar si es un ingreso o un gasto
+    private String category;
 
-    public Entry(Date date, String description, double amount) {
+    public Entry(Date date, String description, double amount, EntryType type, String category) {
         this.date = date;
         this.description = description;
         this.amount = amount;
+        this.type = type;
+        this.category = category;
     }
 
-    // Getter y Setter para date, description y amount
+    // Constructor adicional para simplificar la creación de instancias
+    public Entry(Date date, String description, double amount, EntryType type) {
+        this(date, description, amount, type, null);
+    }
+
+    // Getters y Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public EntryType getType() {
+        return type;
+    }
+
+    public void setType(EntryType type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // Otros métodos según sea necesario
 }
---
+++ src/Model/EntryType.java
@@ -0,0 +1,6 @@
+package Model;
+
+public enum EntryType {
+    INCOME,
+    EXPENSE
+}
