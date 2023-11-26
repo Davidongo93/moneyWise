210632b3ed7f6ba17081e679ab4c3825d0661668
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-11-25 20:28:59 -0500

    User menu

--- src/presentation/MenuHandler.java
+++ src/presentation/MenuHandler.java
@@ -9,9 +9,8 @@ public class MenuHandler {
     private UserManager userManager;
 
     public MenuHandler(Scanner scanner, UserManager userManager) {
-
-        this.userManager =  userManager;
         this.scanner = scanner;
+        this.userManager = userManager;
     }
 
     public void start() {
@@ -33,7 +32,10 @@ public class MenuHandler {
                     userManager.createUser();
                     break;
                 case 2:
-                  userManager.loginUser();
+                    userManager.loginUser();
+                    if (userManager.isUserLoggedIn()) {
+                        showUserMenu();
+                    }
                     break;
                 case 3:
                     System.out.println("Exiting...");
@@ -42,5 +44,38 @@ public class MenuHandler {
                     System.out.println("Invalid option. Please try again.");
             }
         } while (choice != 3);
+    }
+
+    private void showUserMenu() {
+        int choice;
+        do {
+            System.out.println("\nUser Menu:");
+            System.out.println("1. View Entries");
+            System.out.println("2. Create Entry");
+            System.out.println("3. Generate Reports");
+            System.out.println("4. Logout");
+            System.out.print("Select an option: ");
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character after the number
+
+            switch (choice) {
+                case 1:
+                    //NIY userManager.showUserEntries();
+                    break;
+                case 2:
+                   //NIY userManager.createEntry();
+                    break;
+                case 3:
+                    // Logic for generating reports
+                    System.out.println("Generating Reports...");
+                    break;
+                case 4:
+                    userManager.logoutUser();
+                    System.out.println("Logged out successfully.");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        } while (choice != 4);
     }
 }
