0844ac669e443971e4b39ca154a538761d1a5ff4
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-12-08 18:46:30 -0500

    db initializer (Spring not ready yet)

--- src/data/DbInitializer.java
+++ src/data/DbInitializer.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DbInitializer {
-    public static void main(String[] args) {
+    public static void main() {
         createTables();
     }
 
